e4b6e433085b4753f8097651ecd30880151550b1
ManasaAddagatla <maddagat@asu.edu>
2024-06-02 14:43:32 -0500

    updated models.py

--- src/models.py
+++ src/models.py
@@ -36,7 +36,7 @@ config_path = os.path.join(script_dir, '..', 'config', 'config.ini')
 config = configparser.ConfigParser()
 config.read(config_path)
 
-# Set database name
+# Set the database name
 DB_NAME = config['database']['database']
 
 def create_database():
